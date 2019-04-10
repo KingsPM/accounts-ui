fde7d1941c2818a42bd353579289424e6d8472e9
David Brawand <david.brawand@gmail.com>
2019-04-10 12:33:26 +0100

    version bump to 1.3.3

--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
   name: 'std:accounts-ui',
-  version: '1.3.2',
-  summary: 'Accounts UI for React in Meteor 1.3+',
-  git: 'https://github.com/studiointeract/accounts-ui',
+  version: '1.3.3',
+  summary: 'Accounts UI for React in Meteor 1.6+',
+  git: 'https://github.com/kingspm/accounts-ui',
   documentation: 'README.md'
 });
 
